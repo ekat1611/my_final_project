669a3a683cc4a84982a81d80737e2a0be9d85c92
Ekaterina Shiganova <eshiganova@aicorner.org>
2022-11-24 15:57:18 +0400

    add conftest.py, requirements.txt, test_main_page.py, .gitignore

---
+++ conftest.py
@@ -0,0 +1,33 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    # тут парсим из команды переданное в --browser_name значение
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
+    # тут парсим из команды переданное в --language значение
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language: ru or en")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        language = request.config.getoption("language")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+        browser.implicitly_wait(5)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+        browser.implicitly_wait(5)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
